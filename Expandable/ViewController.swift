8708127e8956cc9e3de25c109f220f3efa3d1530
Theint Kyizar <theintkyizar@Theints-Mac.local>
2017-10-26 00:52:04 +0800

    first

--- Expandable/ViewController.swift
+++ Expandable/ViewController.swift
@@ -8,18 +8,155 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: IBOutlet Properties
+    var cellDescriptors: NSMutableArray!
+    var visibleRowsPerSection = [[Int]]()
+    @IBOutlet weak var tblExpandable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureTableView()
+        loadCellDesriptors()
+    }
+    func getIndicesOfVisibleRows() {
+        visibleRowsPerSection.removeAll()
+        
+        for currentSectionCells in cellDescriptors {
+            var visibleRows = [Int]()
+            
+            for row in 0...((currentSectionCells as! [[String: AnyObject]]).count - 1) {
+                if currentSectionCells[row]["isVisible"] as! Bool == true {
+                    visibleRows.append(row)
+                }
+            }
+            
+            visibleRowsPerSection.append(visibleRows)
+        }
+    }
+    
+    func loadCellDesriptors() {
+         if let path = Bundle.main.path(forResource: "CellDescriptor", ofType: "plist") {
+            cellDescriptors = NSMutableArray(contentsOfFile: path)
+            getIndicesOfVisibleRows()
+            tblExpandable.reloadData()
+        }
+       
+    }
+    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
+        let indesOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        let cellDescriptor = cellDescriptors[indexPath.section][indesOfVisibleRow] as! [String: AnyObject]
+        return cellDescriptor
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: Custom Functions
+    
+    func configureTableView() {
+        tblExpandable.delegate = self
+        tblExpandable.dataSource = self
+        tblExpandable.tableFooterView = UIView(frame: CGRect.zero)
+        
+        tblExpandable.register(UINib(nibName: "NormalCell", bundle: nil), forCellReuseIdentifier: "idCellNormal")
+        tblExpandable.register(UINib(nibName: "TextfieldCell", bundle: nil), forCellReuseIdentifier: "idCellTextfield")
+        tblExpandable.register(UINib(nibName: "DatePickerCell", bundle: nil), forCellReuseIdentifier: "idCellDatePicker")
+        tblExpandable.register(UINib(nibName: "SwitchCell", bundle: nil), forCellReuseIdentifier: "idCellSwitch")
+        tblExpandable.register(UINib(nibName: "ValuePickerCell", bundle: nil), forCellReuseIdentifier: "idCellValuePicker")
+        tblExpandable.register(UINib(nibName: "SliderCell", bundle: nil), forCellReuseIdentifier: "idCellSlider")
+    }
 
+    
+    // MARK: UITableView Delegate and Datasource Functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if cellDescriptors != nil {
+            return cellDescriptors.count
+        }
+        else {
+            return 0
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return visibleRowsPerSection[section].count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath:indexPath as NSIndexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: currentCellDescriptor["cellIdentifier"] as! String, for: indexPath as IndexPath) as! CustomCell
+        
+        if currentCellDescriptor["cellIdentifier"] as! String == "idCellNormal" {
+            if let primaryTitle = currentCellDescriptor["primaryTitle"] {
+                cell.textLabel?.text = primaryTitle as? String
+            }
+            
+            if let secondaryTitle = currentCellDescriptor["secondaryTitle"] {
+                cell.detailTextLabel?.text = secondaryTitle as? String
+            }
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellTextfield" {
+            cell.textField.placeholder = currentCellDescriptor["primaryTitle"] as? String
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSwitch" {
+            cell.lblSwitchLabel.text = currentCellDescriptor["primaryTitle"] as? String
+            
+            let value = currentCellDescriptor["value"] as? String
+            cell.swMaritalStatus.isOn = (value == "true") ? true : false
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellValuePicker" {
+            cell.textLabel?.text = currentCellDescriptor["primaryTitle"] as? String
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSlider" {
+            let value = currentCellDescriptor["value"] as! String
+            cell.slExperienceLevel.value = (value as NSString).floatValue
+        }
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Personal"
+        case 1: return "Preferences"
+        default: return "Work Experience"
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: IndexPath) -> CGFloat {
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath: indexPath as NSIndexPath)
+        
+        switch currentCellDescriptor["cellIdentifier"] as! String {
+        case "idCellNormal":
+            return 60.0
+            
+        case "idCellDatePicker":
+            return 270.0
+            
+        default:
+            return 44.0
+        }
+    }
+    
+    
+    @nonobjc func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
+        
+    }
+    
 }
 
